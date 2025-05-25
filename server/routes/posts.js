844b36127080af8042065fa2a9462bebfcf6b3c1
Daksh Billa <dakshbilla2004@gmail.com>
2025-05-26 02:45:23 +0530

    Refactor post creation route to include file upload handling with multer

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,24 @@
+// routes/posts.js
 import express from "express";
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { createPost, getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
+import multer from "multer";
 
 const router = express.Router();
 
-/* READ */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+router.post("/", verifyToken, upload.single("picture"), createPost);
 router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
-
-/* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
 
 export default router;
