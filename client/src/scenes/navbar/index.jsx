ee1cb70f0c1a664ff60459cac776f32903bd7446
Daksh Billa <dakshbilla2004@gmail.com>
2025-05-28 22:28:56 +0530

    backend request url changed

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Social Connect
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
